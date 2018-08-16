49f554c7ed415a68e6631d7322adcec951eb2fa8
Yuri Tceretian <yuri.t@elastic.co>
2018-08-15 21:32:50 -0400

    remove unused attributes

--- config.js
+++ config.js
@@ -3,61 +3,20 @@
  * User Profile
  */
 var profile = {
-  userName: 'saml.jackson@example.com',
-  nameIdFormat: 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
-  firstName: 'Saml',
-  lastName: 'Jackson',
-  displayName: 'saml jackson',
-  email: 'saml.jackson@example.com',
-  mobilePhone: '+1-415-555-5141',
-  groups: 'Simple IdP Users, West Coast Users, Cloud Users'
+  userName: 'saml-test',
+  nameIdFormat: 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent',
+  groups: ['Support', 'Readonly']
 }
 
 /**
  * SAML Attribute Metadata
  */
 var metadata = [{
-  id: "firstName",
-  optional: false,
-  displayName: 'First Name',
-  description: 'The given name of the user',
-  multiValue: false
-}, {
-  id: "lastName",
-  optional: false,
-  displayName: 'Last Name',
-  description: 'The surname of the user',
-  multiValue: false
-}, {
-  id: "displayName",
-  optional: true,
-  displayName: 'Display Name',
-  description: 'The display name of the user',
-  multiValue: false
-}, {
-  id: "email",
-  optional: false,
-  displayName: 'E-Mail Address',
-  description: 'The e-mail address of the user',
-  multiValue: false
-},{
-  id: "mobilePhone",
-  optional: true,
-  displayName: 'Mobile Phone',
-  description: 'The mobile phone of the user',
-  multiValue: false
-}, {
   id: "groups",
-  optional: true,
+  optional: false,
   displayName: 'Groups',
-  description: 'Group memberships of the user',
+  description: 'A list of user roles',
   multiValue: true
-}, {
-  id: "userType",
-  optional: true,
-  displayName: 'User Type',
-  description: 'The type of user',
-  options: ['Admin', 'Editor', 'Commenter']
 }];
 
 module.exports = {
